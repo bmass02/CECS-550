de97c66ecb526d1961097e83fccb775992eba2ad
Shinsetsu <jzach.hte@gmail.com>
2015-09-02 23:37:54 -0400

    Zach - Project Creation .... MVC Web App, Two Core DLL files

---
+++ Huddle/Huddle_Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Huddle_Web.MvcApplication" Language="C#" %>
---
+++ Huddle/Huddle_Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Huddle_Web.Startup))]
+namespace Huddle_Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
